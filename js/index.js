4ae955b3194c652b6540a6d32a5367f542d2a8c8
Prashanth <56287217+PrashuPal@users.noreply.github.com>
2022-03-29 11:52:38 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// typing animation
+var typed = new Typed(".typing",{
+	strings:["","Web Designer","web Developer","Graphic Designer","YouTuber"],
+	typeSpeed:100,
+	backSpeed:30,
+	backDelay: 500,
+	loop:true
+});
